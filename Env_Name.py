18c811cbb3d2a8ced191432be3305288fdff4c20
Qin Yifan <156170866+Qinyifan-hit@users.noreply.github.com>
2025-03-15 21:53:35 +0800

    Create Env_Name.py

---
+++ Env_Name.py
@@ -0,0 +1,16 @@
+Name = {
+    1: "Pendulum-v1",
+    2: "LunarLanderContinuous-v2",
+    3: "Humanoid-v4",
+    4: "HalfCheetah-v4",
+    5: "BipedalWalker-v3",
+    6: "BipedalWalkerHardcore-v3",
+}
+BName = {
+    1: "Pv1",
+    2: "LLCv2",
+    3: "Hv4",
+    4: "HCv4",
+    5: "BWv3",
+    6: "BWHv3",
+}
